d2481aa15d32b05bfd276bff3f701d1dc7d6121b
hide-ki <haraojima1194@gmail.com>
2021-07-29 16:09:25 +0900

    ADD: rubocopの導入、設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def top
-    render html: "hello world"
+    render html: 'hello world'
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,18 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -29,8 +29,17 @@ module MhrizeLayring
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
     config.assets.initialize_on_precompile = false
+    config.generators do |g|
+      g.test_framework :rspec,
+                        view_specs: false,
+                        helper_specs: false,
+                        controller_specs: false,
+                        routing_specs: false,
+                        model_specs: true,
+                        request_specs: false,
+                        fixtures: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
